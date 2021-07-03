ba53183c6bdef0ad30164c2ed08815dc212ccfaa
David Merchan <juan1014166@gmail.com>
2021-07-02 20:31:59 -0500

    Supense used to optimize trend loading

--- src/components/TrendingSearches/LazyTrending.jsx
+++ src/components/TrendingSearches/LazyTrending.jsx
@@ -1,10 +1,20 @@
-import TrendingSearches from './TrendingSearches';
+import React, { Suspense } from 'react';
+
 import useNearScreen from 'hooks/useNearScreen';
+import Spinner from 'components/Spinner/Spinner';
+
+const TrendingSearches = React.lazy(() => import('./TrendingSearches'));
 
 const LazyTrending = () => {
-  const { isNearScreen, fromRef } = useNearScreen();
+  const { isNearScreen, fromRef } = useNearScreen({ distance: '0px' });
 
-  return <div ref={fromRef}>{isNearScreen ? <TrendingSearches /> : null}</div>;
+  return (
+    <div ref={fromRef}>
+      <Suspense fallback={<Spinner />}>
+        {isNearScreen ? <TrendingSearches /> : <Spinner />}
+      </Suspense>
+    </div>
+  );
 };
 
 export default LazyTrending;

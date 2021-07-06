2e78dda0955137deeee4c51e88344a3319daeb21
David Merchan <juan1014166@gmail.com>
2021-07-06 08:26:45 -0500

    avoid renders by separating Search From

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchForm = ({ onSubmit }) => {
+  const [keyword, setKeyword] = useState('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    onSubmit({ keyword });
+  };
+
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button>Buscar</button>
+      <input
+        onChange={handleChange}
+        type="text"
+        value={keyword}
+        placeholder="Search a gif here..."
+      />
+    </form>
+  );
+};
+
+export default SearchForm;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'wouter';
 
 import ListGifs from 'components/ListGifs';
+import SearchForm from 'components/SearchForm';
 import Spinner from 'components/Spinner';
-import TrendingSearches from 'components/TrendingSearches/';
+import TrendingSearches from 'components/TrendingSearches';
 import useGifs from 'hooks/useGifs';
 
 const Home = () => {
-  const [keyword, setKeyword] = useState('');
   // eslint-disable-next-line
   const [path, pushLocation] = useLocation();
-
   const { loading, gifs } = useGifs();
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
+  const handleSubmit = ({ keyword }) => {
     pushLocation(`/search/${keyword}`);
-  };
-
-  const handleChange = (evt) => {
-    setKeyword(evt.target.value);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input
-          onChange={handleChange}
-          type="text"
-          value={keyword}
-          placeholder="Search a gif here..."
-        />
-      </form>
+      <SearchForm onSubmit={handleSubmit} />
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última búsqueda</h3>
